223e6f561bd32469e682c871222818fde15a200e
zenventzi <v.marinov96@gmail.com>
2018-05-10 13:09:11 +0300

    Changing to styled components

--- src/Components/UserProfile/UserAvatar.jsx
+++ src/Components/UserProfile/UserAvatar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import style from './css/avatar.css';
 
 const UserAvatar = () => (
   <div className="grid-item">
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Components/App';
+
+
+import App from './components/main/App';
 
 ReactDOM.render(
   <App message="Hello!" />,
