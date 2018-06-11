414923a9ee24940f4591f5c1e8c7636c552ed2cd
ventzi <v.marinov96@gmail.com>
2018-06-11 08:25:31 +0300

    Fix Prettier Eslint plugin

--- src/components/searchView/User.jsx
+++ src/components/searchView/User.jsx
@@ -1,29 +1,29 @@
 import React from 'react';
 import styled, { ThemeProvider } from 'styled-components';
-import Avatar, { Wrapper as ImgWrapper, Img } from '../reusable/Avatar';
+import Avatar from '../reusable/Avatar';
 import StyledUserName from '../reusable/StyledUserName';
 
 const StyledUser = styled.div`
-display: flex;
-width: 100%;
-flex-wrap: nowrap;
-height: 60px;
-align-items: center;
-justify-content: space-around;
-z-index: 1;
-border-radius: 0.3em; 
-border: 1px solid black;
+  display: flex;
+  width: 100%;
+  flex-wrap: nowrap;
+  height: 60px;
+  align-items: center;
+  justify-content: space-around;
+  z-index: 1;
+  border-radius: 0.3em;
+  border: 1px solid black;
 
-&:hover {
-background: black;
-cursor: pointer;
-${StyledUserName} {
-color: white;
-}
-${Avatar} {
-border-color: white;
-}
-}
+  &:hover {
+    background: black;
+    cursor: pointer;
+    ${StyledUserName} {
+      color: white;
+    }
+    ${Avatar} {
+      border-color: white;
+    }
+  }
 `;
 
 const User = ({ username, avatarSrc }) => {
@@ -41,30 +41,6 @@ const User = ({ username, avatarSrc }) => {
         </StyledUserName>
       </ThemeProvider>
     </StyledUser>
-    // <div>
-    //   <StyledUser>
-    //     <ThemeProvider theme={theme}>
-    //       <ImgWrapper>
-    //           <Img src={avatarSrc} />
-    //       </ImgWrapper>
-    //     </ThemeProvider>
-    //     <StyledUserName>
-    //       {/* {username} */}
-    //       fdfdfdfdfdfdfdfd
-    //     </StyledUserName>
-    //   </StyledUser>
-    //   <StyledUser>
-    //     <ThemeProvider theme={theme}>
-    //       <ImgWrapper>
-    //           <Img src={avatarSrc} />
-    //       </ImgWrapper>
-    //     </ThemeProvider>
-    //     <StyledUserName>
-    //       {/* {username} */}
-    //       fdfdfdfdfdfdfdfdfdfdffdffddffddfdfdfdfdfd
-    //     </StyledUserName>
-    //   </StyledUser>
-    // </div>
   );
 };
 
