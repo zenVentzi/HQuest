c0de4b4e743ae5e1591df41fb49ed38a94172da6
ventzi <v.marinov96@gmail.com>
2018-06-11 08:25:31 +0300

    Fix Prettier Eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,27 @@
 module.exports = {
-    extends: "airbnb",
+    extends: [
+        "airbnb",
+        "prettier",
+        "prettier/react"
+    ],
     rules:{
         "linebreak-style": ["error", "windows"],
-        "jsx-a11y/anchor-is-valid": [ "error", { "components": [ "Link" ], "specialLink": [ "to" ] } ]
+        "jsx-a11y/anchor-is-valid": [ "error", { "components": [ "Link" ], "specialLink": [ "to" ] } ],
+        "prettier/prettier": [
+            "error",
+            {
+                "trailingComma": "es5",
+                "singleQuote": true,
+                "printWidth": 120
+            }
+        ]
     },
     env: {
         "browser": true,
         "es6": true,
     },
     parser: "babel-eslint",
+    plugins: [
+        "prettier"
+    ]
 };
