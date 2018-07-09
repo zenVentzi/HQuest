bcdfad7d941515b2ff8522190a92e87bd0a5edc7
ventzi <v.marinov96@gmail.com>
2018-07-09 16:18:58 +0300

    Update apollo-server to v2

--- server/server.js
+++ server/server.js
@@ -1,12 +1,11 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const bodyParser = require('body-parser');
-const { createServer } = require('http');
-const { graphqlExpress } = require('apollo-server-express');
+const { ApolloServer, makeExecutableSchema } = require('apollo-server');
 const { SubscriptionServer } = require('subscriptions-transport-ws');
 const { execute, subscribe } = require('graphql');
-const { makeExecutableSchema } = require('graphql-tools');
 const jwt = require('express-jwt');
 const webpack = require('webpack');
 const webpackConfig = require('../webpack/config');
@@ -17,18 +16,18 @@ const resolvers = require('./resolvers');
 
 const compiler = webpack(webpackConfig);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 const app = express();
 
-app.use(
-  require('webpack-dev-middleware')(compiler, {
-    hot: true,
-    publicPath: webpackConfig.output.publicPath,
-  })
-);
+// app.use(
+//   require('webpack-dev-middleware')(compiler, {
+//     hot: true,
+//     publicPath: webpackConfig.output.publicPath,
+//   })
+// );
 
-app.use(require('webpack-hot-middleware')(compiler));
-// app.use(express.static(path.join(process.cwd(), 'dist')));
+// app.use(require('webpack-hot-middleware')(compiler));
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(process.cwd(), 'index.html'));
 });
@@ -40,33 +39,42 @@ const auth = jwt({
   credentialsRequired: false,
 });
 
-app.use(
-  '/graphql',
-  bodyParser.json(),
-  auth,
-  graphqlExpress(req => ({
-    schema,
-    context: {
-      user: req.user,
-    },
-  }))
-);
+app.use(bodyParser.json(), auth);
 
-const server = createServer(app);
-
-server.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}...`);
-
-  /* eslint-disable no-new */
-  new SubscriptionServer(
-    {
-      execute,
-      subscribe,
-      schema,
-    },
-    {
-      server,
-      path: '/subscriptions',
+const server = new ApolloServer({
+  schema,
+  context: ({ req, connection }) => {
+    if (connection) {
+      // check connection for metadata
+      return {};
     }
+    return {
+      user: req.user,
+    };
+  },
+});
+server.applyMiddleware({ app });
+
+const httpServer = http.createServer(app);
+server.installSubscriptionHandlers(httpServer);
+
+httpServer.listen(PORT, () => {
+  console.log(
+    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+  );
+  console.log(
+    `🚀 Subscriptions ready at ws://localhost:${PORT}${
+      server.subscriptionsPath
+    }`
   );
 });
+
+// server.listen().then(({ url }) => {
+//   console.log(`🚀 Server ready at ${url}`);
+// });
+
+// app.listen({ port: 4000 }, () => {
+//   console.log(
+//     `🚀 Server ready at http://localhost:${4000}${server.graphqlPath}`
+//   );
+// });
