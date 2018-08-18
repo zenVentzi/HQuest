9e62d7265637d8907fad906bfc5c210b4e278924
ventzi <v.marinov96@gmail.com>
2018-08-18 10:53:33 +0300

    Connect admin view to mongo

--- src/components/adminView/QuestionBody.jsx
+++ src/components/adminView/QuestionBody.jsx
@@ -1,49 +1,101 @@
-import React, { Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
 import styled from 'styled-components';
 import ScaleBody from './ScaleBody';
+import TextBody from './TextBody';
 import OptionsBody from './OptionsBody';
 import WouldYouSingleBody from './WouldYouSingleBody';
 import { QuestionTypes } from './constants';
+
+const CREATE_QUESTION_MUTATION = gql`
+  mutation CreateQuestionMutation(
+    $question: String!
+    $type: QuestionType!
+    $possibleAnswers: [String!]
+  ) {
+    createQuestion(
+      question: $question
+      type: $type
+      possibleAnswers: $possibleAnswers
+    )
+  }
+`;
 
 const CreateBtn = styled.button`
   margin-top: 0.5em;
 `;
 
-const QuestionBody = ({ questionType }) => {
-  const {
-    SCALE,
-    TEXT,
-    OPTIONS,
-    WOULD_YOU_SINGLE,
-    WOULD_YOU_DOUBLE,
-  } = QuestionTypes;
+class QuestionBody extends Component {
+  state = { body: null };
 
-  let Body;
+  onCreate = createQuestion => async () => {
+    const { body } = this.state;
+    const { questionType } = this.props;
 
-  switch (questionType) {
-    case SCALE:
-      Body = ScaleBody;
-      break;
-    case TEXT:
-      break;
-    case OPTIONS:
-      Body = OptionsBody;
-      break;
-    case WOULD_YOU_SINGLE:
-      Body = WouldYouSingleBody;
-      break;
-    case WOULD_YOU_DOUBLE:
-      break;
-    default:
-      return null;
+    const variables = {
+      question: body.question,
+      type: questionType,
+      possibleAnswers: body.possibleAnswers,
+    };
+
+    await createQuestion({ variables });
+    this.props.onAdded();
+  };
+
+  onBodyChange = body => {
+    this.setState({ body });
+  };
+
+  render() {
+    const { questionType } = this.props;
+
+    const {
+      SCALE,
+      TEXT,
+      OPTIONS,
+      WOULD_YOU_SINGLE,
+      WOULD_YOU_DOUBLE,
+    } = QuestionTypes;
+
+    let Body;
+
+    switch (questionType) {
+      case SCALE:
+        Body = ScaleBody;
+        break;
+      case TEXT:
+        Body = TextBody;
+        break;
+      case OPTIONS:
+        Body = OptionsBody;
+        break;
+      case WOULD_YOU_SINGLE:
+        Body = WouldYouSingleBody;
+        break;
+      case WOULD_YOU_DOUBLE:
+        break;
+      default:
+        return null;
+    }
+
+    return (
+      <Mutation mutation={CREATE_QUESTION_MUTATION}>
+        {createQuestion => {
+          const test = 5;
+
+          return (
+            <Fragment>
+              <Body onChange={this.onBodyChange} />
+              <CreateBtn onClick={this.onCreate(createQuestion)}>
+                Create
+              </CreateBtn>
+            </Fragment>
+          );
+        }}
+      </Mutation>
+    );
   }
-
-  return (
-    <Fragment>
-      <Body />
-      <CreateBtn>Create</CreateBtn>
-    </Fragment>
-  );
-};
+}
 
 export default QuestionBody;
