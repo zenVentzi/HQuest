26d6b9a674b2585689d8c1a176b8de1941acd473
ventzi <v.marinov96@gmail.com>
2018-09-05 17:12:35 +0300

    Fix mongoose real-time subscriptions

--- server/PubSub.js
+++ server/PubSub.js
@@ -2,6 +2,9 @@ const { PubSub } = require('graphql-subscriptions');
 
 const pubsub = new PubSub();
 
+const NEW_NOTIFICATION = 'NEW_NOTIFICATION';
+
 module.exports = {
   pubsub,
+  NEW_NOTIFICATION,
 };
