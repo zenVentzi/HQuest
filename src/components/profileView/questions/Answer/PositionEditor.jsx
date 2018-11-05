ffeb271c22fa0bb1ec3b97197f7a51e44ec0a182
ventzi <v.marinov96@gmail.com>
2018-11-05 20:40:30 +0200

    Improve PositionEditor style

--- src/components/profileView/questions/Answer/PositionEditor.jsx
+++ src/components/profileView/questions/Answer/PositionEditor.jsx
@@ -3,6 +3,22 @@ import { toast } from 'react-toastify';
 import styled from 'styled-components';
 import TextBtn from 'Reusable/TextBtn';
 
+const StyledEditor = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+`;
+
+const Buttons = styled.div`
+  display: flex;
+  justify-content: space-between;
+  text-align: center;
+  width: 35%;
+  margin: 1em 0em 1em 0em;
+`;
+
 const PositionEditor = ({
   position,
   maxPosition,
@@ -22,17 +38,19 @@ const PositionEditor = ({
   };
 
   return (
-    <div>
-      Position:{' '}
-      <input
-        onChange={onChange}
-        ref={inputRef}
-        type="number"
-        defaultValue={position}
-        min={1}
-        max={maxPosition}
-      />
+    <StyledEditor>
       <div>
+        Position:{' '}
+        <input
+          onChange={onChange}
+          ref={inputRef}
+          type="number"
+          defaultValue={position}
+          min={1}
+          max={maxPosition}
+        />
+      </div>
+      <Buttons>
         <TextBtn
           onClick={() => {
             const newPosition = parseInt(inputRef.current.value, 10);
@@ -47,9 +65,9 @@ const PositionEditor = ({
         >
           Move
         </TextBtn>
-        <TextBtn onClick={onClickClose}>Close</TextBtn>
-      </div>
-    </div>
+        <TextBtn onClick={onClickClose}>Cancel</TextBtn>
+      </Buttons>
+    </StyledEditor>
   );
 };
 
