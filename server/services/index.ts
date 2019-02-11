3771a802ac49125797a19578bbf228306aaf362f
zenVentzi <v.marinov96@gmail.com>
2019-02-11 17:07:05 +0200

    Refactor userService to use inline function types

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,23 +1,10 @@
-import { AnswerModel } from "../models/answer";
-import { NewsfeedModel } from "../models/newsfeed";
-import { QuestionModel } from "../models/question";
-import { UserModel } from "../models/user";
+import answerService from "./answerService";
+import commentService from "./commentService";
+import newsfeedService from "./newsfeedService";
+import notificationService from "./notificationService";
+import questionService from "./questionService";
+import { userService } from "./userService";
 
-import answerS from "./answerService";
-import commentS from "./commentService";
-import newsfeedS from "./newsfeedService";
-import notificationS from "./notificationService";
-import questionS from "./questionService";
-import userC from "./userService";
-
-const userService = userC(UserModel);
-const questionService = questionS(QuestionModel);
-const answerService = answerS(AnswerModel, UserModel);
-const commentService = commentS(AnswerModel);
-const notificationService = notificationS(UserModel, AnswerModel);
-const newsfeedService = newsfeedS(NewsfeedModel);
-
-// injecting the Models from here in case I want to do testing later
 export {
   userService,
   questionService,
