fe5168f52f8a2befe4ef47b7f388095f8c678050
zenVentzi <v.marinov96@gmail.com>
2019-02-17 16:49:11 +0200

    Cleanup newsfeed model

--- server/models/newsfeed.ts
+++ server/models/newsfeed.ts
@@ -23,34 +23,6 @@ const NewsfeedModel = model<DbTypes.NewsDoc>(
   // "newsfeed" // mongoose puts s at the end, this prevents it
 );
 
-const DocumentModel = model<Document & { toObject(): string }>("doc");
-const docDoc = new DocumentModel();
-const doc = docDoc.toObject();
-
-type Bla = { toObject(): any } & { toObject(): number };
-
-const bla: Bla = {
-  toObject() {
-    return 5;
-  }
-};
-
-const obj = bla.toObject();
-const newsDoc = new NewsfeedModel();
-const news = newsDoc.toObject();
-const newsfeed = await NewsfeedModel.find().lean() as DbTypes.Newsfeed
-
-switch (news.type) {
-  case DbTypes.NewsType.NewFollower:
-    news.
-
-  default:
-    break;
-}
-
-// if(newsfeed.type === DbTypes.NewsType.NewLike) {
-//   newsfeed.an
-// }
 type NewsfeedModel = Model<DbTypes.NewsDoc>;
 
 export { NewsfeedModel };
