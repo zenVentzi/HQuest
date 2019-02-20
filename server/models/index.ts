a79b3341d204819b25d14aeeb0e5f0063ff8b187
zenVentzi <v.marinov96@gmail.com>
2019-02-20 16:21:11 +0200

    Make utils.js to utils.ts and move few functions from server.ts to utils.ts

---
+++ server/models/index.ts
@@ -0,0 +1,11 @@
+import { AnswerModel } from "./answer";
+import { NewsfeedModel } from "./newsfeed";
+import { QuestionModel } from "./question";
+import { UserModel } from "./user";
+
+export const models = {
+  answer: AnswerModel,
+  newsfeed: NewsfeedModel,
+  question: QuestionModel,
+  user: UserModel
+};
