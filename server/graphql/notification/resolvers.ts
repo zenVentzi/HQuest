1b56238b06af3228e401e68f1d974f300f93b1d8
zenVentzi <v.marinov96@gmail.com>
2019-02-22 16:46:33 +0200

    Add missing resolvers for the interface and scalar types

--- server/graphql/notification/resolvers.ts
+++ server/graphql/notification/resolvers.ts
@@ -2,6 +2,16 @@ import { Query, Mutation } from "./types";
 import { mapNotifications } from "./gqlMapper";
 import { notificationService } from "../../services";
 
+const Notification = {
+  __resolveType(obj, context, info) {
+    if (obj.type === "NEW_COMMENT") {
+      return "NewComment";
+    }
+
+    return "NewFollower";
+  }
+};
+
 const Query: Query = {
   async notifications(_, __, context) {
     const dbNotifications = await notificationService.getNotifications(context);
@@ -17,4 +27,4 @@ const Mutation: Mutation = {
   }
 };
 
-export { Query, Mutation };
+export { Notification, Query, Mutation };
