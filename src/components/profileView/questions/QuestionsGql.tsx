bd48694f7534936c3975938c3adb9d5cb68e8dee
zenVentzi <v.marinov96@gmail.com>
2019-02-27 16:53:15 +0200

    Comment out questionsGql.tsx

--- src/components/profileView/questions/QuestionsGql.tsx
+++ src/components/profileView/questions/QuestionsGql.tsx
@@ -1,27 +1,27 @@
-import React, { Component, Fragment } from 'react';
-import { Query, Mutation } from 'react-apollo';
-import { GET_ANSWERED_QUESTIONS, GET_UNANSWERED_QUESTIONS } from 'Queries';
+// import React, { Component, Fragment } from 'react';
+// import { Query, Mutation } from 'react-apollo';
+// import { GET_ANSWERED_QUESTIONS, GET_UNANSWERED_QUESTIONS } from 'Queries';
 
-class QuestionsGql extends Component {
-  render() {
-    const { user, showAnswered, children } = this.props;
-    const { selectedTags } = this.state;
+// class QuestionsGql extends Component {
+//   render() {
+//     const { user, showAnswered, children } = this.props;
+//     const { selectedTags } = this.state;
 
-    const vars = { userId: user.id, tags: selectedTags };
+//     const vars = { userId: user.id, tags: selectedTags };
 
-    const query = showAnswered
-      ? GET_ANSWERED_QUESTIONS
-      : GET_UNANSWERED_QUESTIONS;
+//     const query = showAnswered
+//       ? GET_ANSWERED_QUESTIONS
+//       : GET_UNANSWERED_QUESTIONS;
 
-    return (
-      <Query query={query} variables={vars} fetchPolicy="cache-and-network">
-        {({ loading, error, data: { questions } }) => {
-          // return (<Mutation>{}</Mutation>);
-          return children(loading, error, questions);
-        }}
-      </Query>
-    ); // move the queries here?
-  }
-}
+//     return (
+//       <Query query={query} variables={vars} fetchPolicy="cache-and-network">
+//         {({ loading, error, data: { questions } }) => {
+//           // return (<Mutation>{}</Mutation>);
+//           return children(loading, error, questions);
+//         }}
+//       </Query>
+//     ); // move the queries here?
+//   }
+// }
 
-export default QuestionsGql;
+// export default QuestionsGql;
