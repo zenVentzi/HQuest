a10fe2db8598651bd5bc3d8361c8d7d76c5172b2
zenVentzi <v.marinov96@gmail.com>
2019-02-27 16:55:32 +0200

    Comment out LikePopup file

--- src/components/profileView/questions/LikePopup.tsx
+++ src/components/profileView/questions/LikePopup.tsx
@@ -1,24 +1,29 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+// import React, { Component } from 'react';
+// import styled from 'styled-components';
 
-const LikePopupWrapper = styled.div`
-  position: relative;
-  width: 1.5em;
-  height: 1.5em;
-  background: white;
-  color: black;
-`;
+// const LikePopupWrapper = styled.div`
+//   position: relative;
+//   width: 1.5em;
+//   height: 1.5em;
+//   background: white;
+//   color: black;
+// `;
 
-class LikePopup extends Component {
-  componentDidMount() {
-    setTimeout(() => {
-      this.props.onTimeOut();
-    }, 2000);
-  }
-  render() {
-    const { numOfLikes } = this.props;
-    return <LikePopupWrapper>5</LikePopupWrapper>;
-  }
-}
+// interface LikePopupProps {
+//   onTimeOut: () => void;
+//   numOfLikes: number;
+// }
 
-export default LikePopup;
+// class LikePopup extends Component<LikePopupProps> {
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.props.onTimeOut();
+//     }, 2000);
+//   }
+//   render() {
+//     const { numOfLikes } = this.props;
+//     return <LikePopupWrapper>5</LikePopupWrapper>;
+//   }
+// }
+
+// export default LikePopup;
