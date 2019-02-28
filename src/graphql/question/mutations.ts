4f72c3f9eeaddd5f476991a236eafe61ba2f3a87
zenVentzi <v.marinov96@gmail.com>
2019-02-28 10:52:29 +0200

    Move question queries and mutations into separate folder

---
+++ src/graphql/question/mutations.ts
@@ -0,0 +1,21 @@
+import gql from "graphql-tag";
+import {
+  QuestionFields,
+  CommentFields,
+  AnswerFields
+} from "graphql/gqlFragments";
+
+export const ADD_QUESTIONS = gql`
+  mutation AddQuestionsMutation($questions: [InputQuestion!]) {
+    addQuestions(questions: $questions)
+  }
+`;
+
+export const QUESTION_NOT_APPLY = gql`
+  mutation questionNotApply($questionId: ID!) {
+    questionNotApply(questionId: $questionId) {
+      ...QuestionFields
+    }
+  }
+  ${QuestionFields}
+`;
