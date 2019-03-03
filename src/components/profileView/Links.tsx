75c48caa30a1d0be449aa06d483b989b406032ab
zenVentzi <v.marinov96@gmail.com>
2019-03-03 13:24:35 +0200

    Fix webpack hot reload and styled-icons css prop

--- src/components/profileView/Links.tsx
+++ src/components/profileView/Links.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-
 import styled from "styled-components";
+import * as types from "styled-components/cssprop";
 import { Facebook as FacebookLogo } from "styled-icons/fa-brands/Facebook";
 import { TwitterSquare as TwitterLogo } from "styled-icons/fa-brands/TwitterSquare";
 import { Instagram as InstagramLogo } from "styled-icons/feather/Instagram";
@@ -16,25 +16,21 @@ const StyledLinks = styled.div`
 const Links = () => {
   return (
     <StyledLinks>
-      //@ts-ignore
       <FacebookLogo
         size="2em"
         css="cursor: pointer"
         onClick={() => console.log("click")}
       />
-      //@ts-ignore
       <TwitterLogo
         size="2em"
         css="cursor: pointer"
         onClick={() => console.log("click")}
       />
-      //@ts-ignore
       <InstagramLogo
         size="2em"
         css="cursor: pointer"
         onClick={() => console.log("click")}
       />
-      //@ts-ignore
       <LinkedInLogo
         size="2em"
         css="cursor: pointer"
