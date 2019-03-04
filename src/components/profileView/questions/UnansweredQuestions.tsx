099c1978f29da95c0306f0b78eb317ad9966176b
zenVentzi <v.marinov96@gmail.com>
2019-03-04 09:37:34 +0200

    Hookify UnansweredQuestions.tsx

--- src/components/profileView/questions/UnansweredQuestions.tsx
+++ src/components/profileView/questions/UnansweredQuestions.tsx
@@ -1,4 +1,4 @@
-import React, { Component, Fragment, CSSProperties } from "react";
+import React, { CSSProperties } from "react";
 import { toast } from "react-toastify";
 import styled from "styled-components";
 import UnansweredQuestion from "./UnansweredQuestion";
@@ -9,23 +9,21 @@ interface UnansweredQuestionsProps {
   refetchQuestions: () => Promise<any>;
 }
 
-class UnansweredQuestions extends Component<UnansweredQuestionsProps> {
-  render() {
-    const { style, questions, refetchQuestions } = this.props;
+const UnansweredQuestions = (props: UnansweredQuestionsProps) => {
+  const { style, questions, refetchQuestions } = this.props;
 
-    if (!questions.length) {
-      return <div style={style}> All questions are answered </div>;
-    }
-
-    return questions.map(q => (
-      <UnansweredQuestion
-        key={q.id}
-        style={style}
-        question={q}
-        refetchQuestions={refetchQuestions}
-      />
-    ));
+  if (!questions.length) {
+    return <div style={style}> All questions are answered </div>;
   }
-}
+
+  return questions.map(q => (
+    <UnansweredQuestion
+      key={q.id}
+      style={style}
+      question={q}
+      refetchQuestions={refetchQuestions}
+    />
+  ));
+};
 
 export default UnansweredQuestions;
