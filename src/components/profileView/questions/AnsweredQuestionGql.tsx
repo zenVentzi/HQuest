3350be274d348bdd6005ae9cdcac0ddcd9a48f60
zenVentzi <v.marinov96@gmail.com>
2019-03-04 09:32:50 +0200

    Hookify questions/index.tsx

--- src/components/profileView/questions/AnsweredQuestionGql.tsx
+++ src/components/profileView/questions/AnsweredQuestionGql.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React from "react";
 import { Mutation, MutationFn } from "react-apollo";
 import {
   EDIT_ANSWER,
@@ -16,43 +16,41 @@ interface AnsweredQuestionGqlProps {
   ) => any;
 }
 
-class AnsweredQuestionGql extends Component<AnsweredQuestionGqlProps> {
-  render() {
-    const { children } = this.props;
+const AnsweredQuestionGql = (props: AnsweredQuestionGqlProps) => {
+  const { children } = props;
 
-    // wtf really apollo level pre-japanese
-    // in bulgarian- eб* мааму
-    return (
-      <Mutation mutation={EDIT_ANSWER}>
-        {editAnswer => {
-          return (
-            <Mutation mutation={REMOVE_ANSWER}>
-              {removeAnswer => {
-                return (
-                  <Mutation mutation={MOVE_ANSWER_POSITION}>
-                    {moveAnswerPosition => {
-                      return (
-                        <Mutation mutation={LIKE_ANSWER}>
-                          {likeAnswer => {
-                            return children(
-                              editAnswer,
-                              removeAnswer,
-                              moveAnswerPosition,
-                              likeAnswer
-                            );
-                          }}
-                        </Mutation>
-                      );
-                    }}
-                  </Mutation>
-                );
-              }}
-            </Mutation>
-          );
-        }}
-      </Mutation>
-    );
-  }
-}
+  // wtf really apollo level pre-japanese
+  // in bulgarian- eб* мааму
+  return (
+    <Mutation mutation={EDIT_ANSWER}>
+      {editAnswer => {
+        return (
+          <Mutation mutation={REMOVE_ANSWER}>
+            {removeAnswer => {
+              return (
+                <Mutation mutation={MOVE_ANSWER_POSITION}>
+                  {moveAnswerPosition => {
+                    return (
+                      <Mutation mutation={LIKE_ANSWER}>
+                        {likeAnswer => {
+                          return children(
+                            editAnswer,
+                            removeAnswer,
+                            moveAnswerPosition,
+                            likeAnswer
+                          );
+                        }}
+                      </Mutation>
+                    );
+                  }}
+                </Mutation>
+              );
+            }}
+          </Mutation>
+        );
+      }}
+    </Mutation>
+  );
+};
 
 export default AnsweredQuestionGql;
