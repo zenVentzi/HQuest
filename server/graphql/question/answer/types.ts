cfe52c8fc713b00e9a727cd1f76b2de8a38e2ec6
zenVentzi <v.marinov96@gmail.com>
2019-03-29 17:55:12 +0200

    graphql answer use autogen types

--- server/graphql/question/answer/types.ts
+++ server/graphql/question/answer/types.ts
@@ -1,79 +1,79 @@
-import { Resolver } from "../../global/types";
-import { User } from "../../user/types";
-import { Comment } from "../../autoGenTypes";
-import { ApolloContext } from "gqlContext";
+// import { Resolver } from "../../global/types";
+// import { User } from "../../user/types";
+// import { Comment } from "../../autoGenTypes";
+// import { ApolloContext } from "gqlContext";
 
-export interface Answer {
-  id: string;
-  questionId: string;
-  editions: AnswerEdition[];
-  position: number;
-  userId: string;
-}
+// export interface Answer {
+//   id: string;
+//   questionId: string;
+//   editions: AnswerEdition[];
+//   position: number;
+//   userId: string;
+// }
 
-export interface Likes {
-  total: number;
-  likers: Liker[];
-}
+// export interface Likes {
+//   total: number;
+//   likers: Liker[];
+// }
 
-export interface Liker {
-  user: User;
-  numOfLikes: number;
-}
+// export interface Liker {
+//   user: User;
+//   numOfLikes: number;
+// }
 
-export interface AnswerEdition {
-  id: string;
-  date: Date;
-  value: string;
-  comments?: Comment[] | null;
-  likes?: Likes | null;
+// export interface AnswerEdition {
+//   id: string;
+//   date: Date;
+//   value: string;
+//   comments?: Comment[] | null;
+//   likes?: Likes | null;
 
-  // before: string;
+//   // before: string;
 
-  // after: string;
-}
+//   // after: string;
+// }
 
-// export interface Query {}
+// // export interface Query {}
 
-interface EditAnswerArgs {
-  answerId: string;
-  answerValue: string;
-}
+// interface EditAnswerArgs {
+//   answerId: string;
+//   answerValue: string;
+// }
 
-interface AddAnswerArgs {
-  questionId: string;
-  answerValue: string;
-}
+// interface AddAnswerArgs {
+//   questionId: string;
+//   answerValue: string;
+// }
 
-interface RemoveAnswerArgs {
-  answerId: string;
-}
+// interface RemoveAnswerArgs {
+//   answerId: string;
+// }
 
-interface LikeAnswerEditionArgs {
-  answerId: string;
-  answerEditionId: string;
-  userLikes: number;
-}
+// interface LikeAnswerEditionArgs {
+//   answerId: string;
+//   answerEditionId: string;
+//   userLikes: number;
+// }
 
-interface MoveAnswerPositionArgs {
-  answerId: string;
-  position: number;
-}
+// interface MoveAnswerPositionArgs {
+//   answerId: string;
+//   position: number;
+// }
 
-export interface Mutation {
-  editAnswer: Resolver<{}, EditAnswerArgs, ApolloContext, Answer>;
-  addAnswer: Resolver<{}, AddAnswerArgs, ApolloContext, Answer>;
-  removeAnswer: Resolver<{}, RemoveAnswerArgs, ApolloContext, Answer>;
-  likeAnswerEdition: Resolver<
-    {},
-    LikeAnswerEditionArgs,
-    ApolloContext,
-    AnswerEdition
-  >;
-  moveAnswerPosition: Resolver<
-    {},
-    MoveAnswerPositionArgs,
-    ApolloContext,
-    number
-  >;
-}
+// export interface Mutation {
+//   editAnswer: Resolver<{}, EditAnswerArgs, ApolloContext, Answer>;
+//   addAnswer: Resolver<{}, AddAnswerArgs, ApolloContext, Answer>;
+//   removeAnswer: Resolver<{}, RemoveAnswerArgs, ApolloContext, Answer>;
+//   likeAnswerEdition: Resolver<
+//     {},
+//     LikeAnswerEditionArgs,
+//     ApolloContext,
+//     AnswerEdition
+//   >;
+//   moveAnswerPosition: Resolver<
+//     {},
+//     MoveAnswerPositionArgs,
+//     ApolloContext,
+//     number
+//   >;
+// }
