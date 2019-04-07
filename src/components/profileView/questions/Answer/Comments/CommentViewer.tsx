081ff5749a84c5c325979ee40fc7a68b94f1800c
zenVentzi <v.marinov96@gmail.com>
2019-04-07 21:20:51 +0300

    improve commentViewer to show links to mentioned profiles

--- src/components/profileView/questions/Answer/Comments/CommentViewer.tsx
+++ src/components/profileView/questions/Answer/Comments/CommentViewer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import { mentionRegex } from "./MentionConstants";
+import { Link } from "react-router-dom";
 
 const StyledViewer = styled.p`
   word-break: break-all;
@@ -14,7 +16,37 @@ interface CommentViewerProps {
 
 const CommentViewer = (props: CommentViewerProps) => {
   const { comment } = props;
-  return <StyledViewer>- {comment} </StyledViewer>;
+  const splitComment = comment.split(mentionRegex);
+  const hasMentions = splitComment.length > 0;
+  if (hasMentions) {
+    return (
+      <StyledViewer>
+        -{" "}
+        {splitComment.map(text => {
+          const isMention = text.search(mentionRegex) !== -1;
+          if (!isMention) return text;
+          const userIdRegex = text.match(/\(\w+\)/);
+          const usernameRegex = text.match(/\[\w+( \w+)*\]/);
+          if (!userIdRegex || !usernameRegex) {
+            throw Error(`incorrect mention syntax`);
+          }
+          const userId = userIdRegex[0].slice(1, -1);
+          const username = usernameRegex[0].slice(1, -1);
+          const linkToUserProfile = `/userprofile/${userId}`;
+
+          console.log(username);
+
+          return (
+            <Link to={linkToUserProfile} key={userId}>
+              {username}
+            </Link>
+          );
+        })}
+      </StyledViewer>
+    );
+  }
+
+  return <StyledViewer>- {comment}</StyledViewer>;
 };
 
 export default CommentViewer;
---
+++ src/components/profileView/questions/Answer/Comments/MentionConstants.ts
@@ -0,0 +1,2 @@
+const mentionRegex = /(@\[\w+(?: \w+)*\]\(\w+\))/;
+export { mentionRegex };
