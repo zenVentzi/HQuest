c86b41b000d302bc3f77cf807bcc6457ad23df71
zenVentzi <v.marinov96@gmail.com>
2019-04-08 10:28:17 +0300

    fix add new mentions on comment edit

--- src/components/profileView/questions/Answer/Comments/CommentEditor.tsx
+++ src/components/profileView/questions/Answer/Comments/CommentEditor.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Textarea from "react-textarea-autosize";
 import { Formik, Form, ErrorMessage } from "formik";
 import styled from "styled-components";
 import TextBtn from "Reusable/TextBtn";
+import MentionInput from "./MentionInput";
 // import { EditionFieldsComments } from "GqlClient/autoGenTypes";
 
 const StyledEditor = styled(Textarea)`
@@ -32,59 +33,48 @@ const RightBtn = styled(TextBtn)``;
 
 interface CommentEditorProps {
   comment: string;
-  onEdit: (newComment: string) => Promise<void>;
+  onEdit: (
+    newComment: string,
+    mentionedUserIds: string[] | undefined
+  ) => Promise<{ success: boolean }>;
   onCancel: () => void;
+  searchUsers: any;
 }
 
 /* this component is pretty much the same as AnswerEditor and CommentInput. DRY fix */
 
 const CommentEditor = (props: CommentEditorProps) => {
-  const formInitialValues = { comment: props.comment };
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [shouldSubmit, setShouldSubmit] = useState(false);
+
+  /* 1) pass forceSubmit property down to MentionInput
+  2) Move the logic from onChange one layer up.
+  3) Expose callbacks for onValueChange and onMentionsChange
+    - this means I'll keep state in 2 places */
 
   return (
-    <Formik
-      initialValues={formInitialValues}
-      validateOnBlur={false}
-      validate={values => {
-        const errors: any = {};
-        if (values.comment.length < 7) {
-          errors.comment = "Comment must be at least 7 characters";
-        }
-
-        return errors;
-      }}
-      onSubmit={async (values, { setSubmitting, resetForm }) => {
-        await props.onEdit(values.comment);
-        // setSubmitting(false);
-        // resetForm(formInitialValues);
-      }}
-    >
-      {({ values, handleChange, submitForm, handleBlur, isSubmitting }) => (
-        <Form style={{ width: "100%", textAlign: "center" }}>
-          <StyledEditor
-            name="comment"
-            onChange={handleChange}
-            onBlur={handleBlur}
-            value={values.comment}
-            disabled={isSubmitting}
-            onKeyPress={e => {
-              if (e.key === "Enter" && !e.shiftKey) {
-                e.preventDefault();
-                if (!isSubmitting) submitForm();
-              }
-            }}
-          />
-          <ErrorMessage
-            name="comment"
-            render={msg => <ErrorText>{msg}</ErrorText>}
-          />
-          <Buttons>
-            <LeftBtn onClick={submitForm}>Save</LeftBtn>
-            <RightBtn onClick={props.onCancel}>Cancel</RightBtn>
-          </Buttons>
-        </Form>
-      )}
-    </Formik>
+    <>
+      <MentionInput
+        initialValue={props.comment}
+        searchUsers={props.searchUsers}
+        submitOnEnter={true}
+        shouldSubmit={shouldSubmit}
+        isSubmitting={isSubmitting}
+        onSubmit={props.onEdit}
+      />
+      <Buttons>
+        <LeftBtn
+          onClick={() => {
+            if (!isSubmitting) {
+              setShouldSubmit(true);
+            }
+          }}
+        >
+          Save
+        </LeftBtn>
+        <RightBtn onClick={props.onCancel}>Cancel</RightBtn>
+      </Buttons>
+    </>
   );
 };
 
